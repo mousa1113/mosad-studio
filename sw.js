9a76097acf5842f5c059fb29e5934a4f8c03b4bf
mousa1113 <mohmosa125@gmail.com>
2026-01-31 19:08:02 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'mosad-studio-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/logo.png'
+];
+
+// تثبيت الـ Service Worker وحفظ الملفات في الكاش
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// استرجاع الملفات من الكاش عند انقطاع الإنترنت
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
